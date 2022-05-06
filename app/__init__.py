d446f63ba43eed5b8ef167a16ccb3e1ee2e52375
Michael Ogaye <hogaeymic@gmail.com>
2022-05-07 02:07:02 +0300

    ft: importing the views into the blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -20,3 +20,18 @@ photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
     app=Flask(__name__)
     app.config.from_object(config_options[config_name])
+    from .auth import auth as authentication_blueprint
+    from .main import main as main_blueprint
+
+    app.register_blueprint(authentication_blueprint)
+    app.register_blueprint(main_blueprint)
+
+    login_manager.init_app(app)
+    db.init_app(app)
+    bootstap.init_app(app)
+    configure_uploads(app,photos)
+    mail.init_app(app)
+    
+
+
+    return app
