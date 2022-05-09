d7f6b969fd38b5f61d3d7e91541ed244803e6da4
Michael Ogaye <hogaeymic@gmail.com>
2022-05-10 01:18:52 +0300

    crimson

--- create_db.py
+++ create_db.py
@@ -2,7 +2,7 @@ import os
 from app import db,create_app
 # from flask_migrate import migrate
 app=create_app('production')
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://zsrkkslissrebb:db4e439bc8352672913c44bb5e2f5baa42603453446e1055b6afc138a19b400e@ec2-52-200-215-149.compute-1.amazonaws.com:5432/dbermpdgukmjpq'
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://ugwvtsmiuritaa:dda5ba73451268789df57d15662955dbb69d15119135639cb8c4dafccfd13208@ec2-54-172-175-251.compute-1.amazonaws.com:5432/dep65eldusiv8i'
 db.init_app(app)
 
 with app.app_context():
