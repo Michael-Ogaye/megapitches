fd16416716d9cf48de26ba8a3f0a7389031e113e
Michael Ogaye <hogaeymic@gmail.com>
2022-05-10 01:00:11 +0300

    env

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config:
 
 
 class ProdConfig(Config):
-    DATABASE_URL="postgres://zsrkkslissrebb:db4e439bc8352672913c44bb5e2f5baa42603453446e1055b6afc138a19b400e@ec2-52-200-215-149.compute-1.amazonaws.com:5432/dbermpdgukmjpq"
+    SQLACHEMY_DATABASE_URI="postgres://zsrkkslissrebb:db4e439bc8352672913c44bb5e2f5baa42603453446e1055b6afc138a19b400e@ec2-52-200-215-149.compute-1.amazonaws.com:5432/dbermpdgukmjpq"
 
 
 class DevConfig(Config):
