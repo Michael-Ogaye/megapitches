ba67783abea632aaa4d712bc05d12e610719d5dc
Michael Ogaye <hogaeymic@gmail.com>
2022-05-09 22:47:21 +0300

    withcontextadded

--- create_db.py
+++ create_db.py
@@ -1,7 +1,9 @@
 from app import db,create_app
-from flask_migrate import migrate
+# from flask_migrate import migrate
 app=create_app('production')
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
 
-db.create_all()
+
